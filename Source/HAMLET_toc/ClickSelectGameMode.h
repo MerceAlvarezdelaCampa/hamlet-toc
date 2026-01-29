f7246876835cfdb47d47337501fe53ddab310096
MerceAlvarezdelaCampa <m.adlcc93@gmail.com>
2026-01-29 15:08:14 +0200

    Add click selection player controller

---
+++ Source/HAMLET_toc/ClickSelectGameMode.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClickSelectGameMode.generated.h"
+
+UCLASS()
+class HAMLET_TOC_API AClickSelectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AClickSelectGameMode();
+};
---
+++ Source/HAMLET_toc/ClickSelectPlayerController.cpp
@@ -0,0 +1,79 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ClickSelectPlayerController.h"
+
+#include "Engine/EngineTypes.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+#include "Components/PrimitiveComponent.h"
+
+void AClickSelectPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+}
+
+void AClickSelectPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (InputComponent)
+	{
+		InputComponent->BindAction("Select", IE_Pressed, this, &AClickSelectPlayerController::HandleSelectPressed);
+	}
+}
+
+void AClickSelectPlayerController::HandleSelectPressed()
+{
+	FHitResult HitResult;
+	const ETraceTypeQuery TraceChannel = UEngineTypes::ConvertToTraceType(ECC_Visibility);
+	const bool bHit = GetHitResultUnderCursorByChannel(TraceChannel, true, HitResult);
+	AActor* HitActor = bHit ? HitResult.GetActor() : nullptr;
+
+	UpdateSelection(HitActor);
+}
+
+void AClickSelectPlayerController::UpdateSelection(AActor* NewSelection)
+{
+	if (SelectedActor == NewSelection)
+	{
+		return;
+	}
+
+	if (SelectedActor)
+	{
+		SetActorSelected(SelectedActor, false);
+	}
+
+	SelectedActor = NewSelection;
+
+	if (SelectedActor)
+	{
+		SetActorSelected(SelectedActor, true);
+		UE_LOG(LogTemp, Log, TEXT("Selected actor: %s"), *SelectedActor->GetName());
+	}
+}
+
+void AClickSelectPlayerController::SetActorSelected(AActor* Actor, bool bSelected) const
+{
+	if (!Actor)
+	{
+		return;
+	}
+
+	TArray<UActorComponent*> Components = Actor->GetComponentsByClass(UPrimitiveComponent::StaticClass());
+	for (UActorComponent* Component : Components)
+	{
+		if (UPrimitiveComponent* PrimitiveComponent = Cast<UPrimitiveComponent>(Component))
+		{
+			PrimitiveComponent->SetRenderCustomDepth(bSelected);
+			if (bSelected)
+			{
+				PrimitiveComponent->SetCustomDepthStencilValue(1);
+			}
+		}
+	}
+}
