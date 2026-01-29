166e968fa2b684a3b6ea6baca16ef7a5ae119b18
MerceAlvarezdelaCampa <m.adlcc93@gmail.com>
2026-01-29 18:08:47 +0200

    Allow runtime outline color updates

--- Source/HAMLET_toc/ClickSelectPlayerController.h
+++ Source/HAMLET_toc/ClickSelectPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "ClickSelectPlayerController.generated.h"
 
+class UMaterialInstanceDynamic;
+class UMaterialInterface;
+
 UCLASS()
 class HAMLET_TOC_API AClickSelectPlayerController : public APlayerController
 {
@@ -15,11 +18,27 @@ public:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
 
+	UFUNCTION(BlueprintCallable, Category = "Selection|Outline")
+	void SetSelectionOutlineColor(const FLinearColor& NewColor);
+
 private:
+	UPROPERTY(EditDefaultsOnly, Category = "Selection|Outline", meta = (ClampMin = "1", ClampMax = "255"))
+	int32 SelectionStencilValue = 1;
+
+	UPROPERTY(EditAnywhere, Category = "Selection|Outline")
+	FLinearColor SelectionOutlineColor = FLinearColor(1.0f, 0.5f, 0.0f, 1.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Selection|Outline")
+	TObjectPtr<UMaterialInterface> SelectionOutlineMaterial;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> SelectionOutlineMID;
+
 	UPROPERTY()
 	TObjectPtr<AActor> SelectedActor;
 
 	void HandleSelectPressed();
 	void UpdateSelection(AActor* NewSelection);
 	void SetActorSelected(AActor* Actor, bool bSelected) const;
+	void ConfigureOutlinePostProcess();
 };
