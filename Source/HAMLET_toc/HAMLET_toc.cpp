d90f7ea5e2fbdca9287676a85021e504b78db66e
Merce Alvarez de la Campa <m.alvarez@cyens.org>
2026-01-29 14:51:32 +0200

    Initial commit: Project setup with LFS and Ignore rules

---
+++ Source/HAMLET_toc/HAMLET_toc.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HAMLET_toc.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HAMLET_toc, "HAMLET_toc" );
